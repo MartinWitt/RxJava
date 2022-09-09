c4cc8aea6898c31920b3663d8f51dfa5d78ddefc
MartinWitt <wittlinger.martin@gmail.com>
2022-09-09 00:45:54 +0000

    fix Bad Smells in io.reactivex.rxjava3.internal.operators.observable.ObservableGroupBy$GroupedUnicast

--- src/main/java/io/reactivex/rxjava3/internal/operators/observable/ObservableGroupBy.java
+++ src/main/java/io/reactivex/rxjava3/internal/operators/observable/ObservableGroupBy.java
@@ -195,13 +195,13 @@ public final class ObservableGroupBy<T, K, V> extends AbstractObservableWithUpst
             return new GroupedUnicast<>(key, state);
         }
 
-        protected GroupedUnicast(K key, State<T, K> state) {
+        GroupedUnicast(K key, State<T, K> state) {
             super(key);
             this.state = state;
         }
 
         @Override
-        protected void subscribeActual(Observer<? super T> observer) {
+void subscribeActual(Observer<? super T> observer) {
             state.subscribe(observer);
         }
 
